5585c24b6b9026b7b5701a6ecdbff38ee99410b5
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-11-08 12:44:54 -0600

    all done

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const catRoutes = require('./app/routes/cat_routes')
 
